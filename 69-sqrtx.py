e015c4d1e2400ec84258bf9086b6c4e9cee24d06
Muhammet U <73366454+mu-us61@users.noreply.github.com>
2025-10-08 13:48:49 +0300

    Create 69-sqrtx.py

---
+++ 69-sqrtx.py
@@ -0,0 +1,11 @@
+# 69
+# https://leetcode.com/problems/sqrtx
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        i=1
+        while i*i <= x:
+            i=i+1
+        return i-1 
+
+        
